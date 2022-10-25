c8a3db225c98b1f652ca2781603b9d1b0801dbf7
FreakeyPlays <chrismerck@gmx.de>
2022-10-25 16:39:33 +0200

    feat(api): swagger api documentation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder } from '@nestjs/swagger'
+import { SwaggerModule } from '@nestjs/swagger/dist'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo API')
+    .setDescription('Documentation to the Todo API.')
+    .setVersion('1.0')
+    .addTag('todo')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
