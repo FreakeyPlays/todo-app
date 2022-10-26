f763439ab3e8844668e8d994df70590881abc5fb
FreakeyPlays <chrismerck@gmx.de>
2022-10-26 16:47:08 +0200

    feat(env): injectet API with .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Todo } from './todo/models/todo.entity'
 import { TodoModule } from './todo/todo.module'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 54321,
-      username: 'postgres',
-      password: 'password',
-      database: 'postgres',
+      host: process.env.HOST || 'localhost',
+      port: parseInt(process.env.POSTGRES_PORT) || 54321,
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'password',
+      database: process.env.postgres || 'postgres',
       entities: [Todo],
       synchronize: true
     }),
