c2b87d8836141081bce0f8c5ce2602e5fff181aa
FreakeyPlays <chrismerck@gmx.de>
2022-11-04 14:23:50 +0100

    docs(swagger): added API descriptions

--- server/src/todo/controller/todo.controller.ts
+++ server/src/todo/controller/todo.controller.ts
@@ -8,7 +8,16 @@ import {
   Post,
   Put
 } from '@nestjs/common'
-import { ApiBody, ApiTags } from '@nestjs/swagger'
+import {
+  ApiAcceptedResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags
+} from '@nestjs/swagger'
 import { DeleteResult, UpdateResult } from 'typeorm'
 import { T_Todo } from '../models/todo.interface'
 import { TodoService } from '../service/todo.service'
@@ -43,16 +52,28 @@ export class TodoController {
     }
   })
   @Post()
+  @ApiOperation({
+    summary: 'Create ToDo in the Database with content from Body'
+  })
+  @ApiCreatedResponse({ description: 'ToDo was Created in the Database' })
   public createTodo(@Body() todo: T_Todo): Promise<T_Todo> {
     return this.service.addOne(todo)
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Returns all ToDos from Database'
+  })
+  @ApiOkResponse({ description: 'All ToDos are Returned from the Database' })
   public findAllTodos(): Promise<T_Todo[]> {
     return this.service.findAll()
   }
 
   @Get('/:id')
+  @ApiOperation({
+    summary: 'Returns ToDo from Database by ID from Path variable'
+  })
+  @ApiOkResponse({ description: 'ToDo was Returned from the Database' })
   public findTodoByName(@Param('id') id: number): Promise<T_Todo> {
     return this.service.findOne(id)
   }
@@ -78,6 +99,13 @@ export class TodoController {
     }
   })
   @Put('/:id')
+  @ApiOperation({
+    summary: 'Updates ToDo by ID from Path, with content from Body'
+  })
+  @ApiOkResponse({ description: 'ToDo was Updated in the Database' })
+  @ApiNoContentResponse({
+    description: 'ToDo was Updated in the Database, but got no Response'
+  })
   public updateTodo(
     @Param('id') id: number,
     @Body() todo: T_Todo
@@ -86,6 +114,12 @@ export class TodoController {
   }
 
   @Delete('/:id')
+  @ApiOperation({ summary: 'Deletes ToDo from Database by ID from Path.' })
+  @ApiOkResponse({ description: 'ToDo was Deleted from the Database' })
+  @ApiAcceptedResponse({ description: 'ToDo will be Deleted from Database' })
+  @ApiNoContentResponse({
+    description: 'ToDo was Deleted from the Database, but got no Response'
+  })
   public deleteTodo(@Param('id') id: number): Promise<DeleteResult> {
     return this.service.removeOne(id)
   }
