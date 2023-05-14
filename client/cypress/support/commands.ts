e113074dc97317e9de0f4ff21cba73f316736d44
Chris Merck <chrismerck@gmx.de>
2023-05-14 13:54:30 +0200

    test(e2e): added custom testing functions

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,37 +1,42 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+export {}
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      createTodo(
+        name: string,
+        priority: VALID_PRIORITIES,
+        done: boolean
+      ): Chainable<void>
+      deleteAllTodos(): Chainable<void>
+    }
+  }
+}
+
+type VALID_PRIORITIES = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+
+Cypress.Commands.add(
+  'createTodo',
+  (name: string, priority: VALID_PRIORITIES, done: boolean) => {
+    cy.get('app-template-todo-form input[type="text"]').type(name)
+    cy.get('app-template-todo-form select').select(priority)
+    cy.get('app-template-todo-form div.todo-create').click()
+
+    if (!done) return
+
+    cy.get('div.todo-entry-list')
+      .eq(0)
+      .last()
+      .find('div.todo-status-check')
+      .click()
+  }
+)
+
+Cypress.Commands.add('deleteAllTodos', () => {
+  cy.get('app-template-todo')
+    .should('have.length.at.least', 0)
+    .each($todo => {
+      cy.wrap($todo).find('div.todo-delete').click()
+    })
+})
